d60cccea50d3fb2fcfe281f01af04f227dcf0f75
jodevera <jodevera@deloitte.com>
2022-05-02 19:21:36 +0800

    update discovery page

--- src/components/Pages/Discover.js
+++ src/components/Pages/Discover.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import './styles/discover.css';
 import chevronImg from '../../assets/destination/down-chevron.svg';
 
@@ -6,49 +6,19 @@ import chevronImg from '../../assets/destination/down-chevron.svg';
 
 const Discover = () => {
 
-    const showSearch = {
-        transform: 'translateY(0%)',
-        display: 'block'
-    };
-    const hideSearch = {
-        transform: 'translateY(100%)',
-    };
-    const hideChev = {
-        display: 'none'
-    }
-    const showChev = {
-        display: 'block'
-    }
+    //setup APIs
 
-    const hideImg = {
-        display: 'none'
-    }
-
-    const [searchStatus, setSearchStatus] = useState(hideSearch);
-    const [upChevStatus, setUpChevStatus] = useState(showChev);
-    const [downChevStatus, setDownChevStatus] = useState(hideChev);
-
-    const [patchStatus, setPatchStatus] = useState();
-
-
-    const clickUpChevron = () => {
-        setSearchStatus(showSearch);
-        setUpChevStatus(hideChev);
-        setDownChevStatus(showChev);
-    }
-
-    const clickDownChevron = () => {
-        setSearchStatus(hideSearch);
-        setUpChevStatus(showChev);
-        setDownChevStatus(hideChev);
-    }
-    const API_URL = "http://localhost:3500/launches";
+    const LAUNCH_URL = "http://localhost:3500/launches";
+    const LAUNCHPAD_URL = "http://localhost:3600/launchpads";
     const [launches, setLaunches] = useState([])
- 
+    const [launchPads, setLaunchPads] = useState([])
+    
+    //get launch API Response
+
     useEffect(() => {
         const fetchLaunches = async () => {
             try {
-                const response = await fetch(API_URL);
+                const response = await fetch(LAUNCH_URL);
                 const listLaunches = await response.json();
                 setLaunches(listLaunches);
             } catch (err) {
@@ -57,7 +27,77 @@ const Discover = () => {
         }
         (async () => await fetchLaunches())();
     }, [])
+    //get launchpad API Response
 
+    useEffect(() => {
+        const fetchLaunchPads = async () => {
+            try {
+                const responseLP = await fetch(LAUNCHPAD_URL);
+                const listLaunchPads = await responseLP.json();
+                setLaunchPads(listLaunchPads);
+            } catch (err) {
+                console.log(err.stack)
+            }
+        }
+        (async () => await fetchLaunchPads())();
+    }, [])
+
+    //scroll to search container
+
+    const launchRef = useRef(null);
+
+    const handleClick = () => {
+        launchRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+
+
+    //keyword filter
+
+    const [keyword, setKeyword] = useState("");
+    const [lpFilter, setLpFilter] = useState("");
+
+    const handleKeywordChange = event => {
+        setKeyword(event.target.value)
+    };
+
+    const handleLPChange = event => {
+        setLpFilter(event.target.value)
+    };
+
+    const lowercasedKeyword = keyword.toString().toLowerCase();
+    const lowercasedLpFilter = lpFilter.toString().toLowerCase();
+
+    console.log("keyword: " + lowercasedKeyword);
+    console.log("launchpad: " + lowercasedLpFilter);
+
+    const filteredLaunches = launches.filter(launch => {
+
+        return(
+            //KEYWORD FILTER
+            (//check launch array
+            Object.keys(launch).some(key =>
+                typeof launch[key] === "string" && (launch[key].toLowerCase().includes(lowercasedKeyword))) ||
+            //check rocket attributes
+            Object.keys(launch.rocket).some(key =>
+                typeof launch.rocket[key] === "string" && (launch.rocket[key].toLowerCase().includes(lowercasedKeyword)))  ||
+            //check payloads
+            Object.keys(launch.payloads).some(key =>
+                typeof launch.payloads[key] === "string" && (launch.payloads[key].toLowerCase().includes(lowercasedKeyword)))  ||
+            //check launchpads while matching with launchpad API
+            Object.keys(launch.launch_site).some(key =>
+                typeof launch.launch_site[key] === "string" && (launchPads.filter((launchpad) => launch?.launch_site?.site_id === launchpad?.id)[0]?.full_name.toLowerCase().includes(lowercasedKeyword)))) &&
+            
+            //LAUNCHPAD FILTER
+            //check launchpads via launchpad filter    
+            Object.keys(launch.launch_site).some(key =>
+                typeof launch.launch_site[key] === "string" && (launchPads.filter((launchpad) => launch?.launch_site?.site_id === launchpad?.id)[0]?.full_name.toLowerCase().includes(lowercasedLpFilter)))
+        );
+    });
+
+    //get no. of missions
+    var noOfMissions = filteredLaunches.length;
+
+    
     return(
         <>
             <div className = "subtitle heading5"><span className = "number">04</span>&nbsp;REVISIT YOUR JOURNIES</div>
@@ -66,55 +106,57 @@ const Discover = () => {
                     DISCOVER SPACE MISSIONS
                 </div>
                 <div className  = "discoverIcon">
-                    <img onClick = { () => clickUpChevron() } className = "discoverChevUpSrc" id = "destImg" src = {chevronImg} style = {upChevStatus} alt = "chevron" />
-                    <img onClick = { () => clickDownChevron() } className = "discoverChevDownSrc" id = "destImg" src = {chevronImg} style = {downChevStatus} alt = "chevron" />
+                    <img onClick = { () => handleClick() } className = "discoverChevDownSrc" id = "destImg" src = {chevronImg} alt = "chevron" />
                 </div>  
             </div>
-            <div className = "discoverSearch" style = {searchStatus}>
-                <div className = "discoverInputs">
-                    <div className = "discoverInputContainer">
-                        <div className = "discoverSearchInput">
-                            <label for = "keyword" className = "discoverLabel bodytext">Keywords</label>
-                            <input className = "discoverTextBox" id = "keyword" type = "text" placeholder = "eg Falcon"/>
-                        </div>
-                        <div className = "discoverSearchInput">
-                            <label for = "launchpad" className = "discoverLabel bodytext">Launchpad</label>
-                            <input className = "discoverTextBox" id = "launchpad" type = "text" />
-                        </div>
-                        <div className = "discoverSearchInput">
-                            <label for = "minyear" className = "discoverLabel bodytext">Min. Year</label>
-                            <input className = "discoverTextBox" id = "minyear" type = "text" />
-                        </div>
-                        <div className = "discoverSearchInput">
-                            <label for = "maxyear" className = "discoverLabel bodytext">Max. Year</label>
-                            <input className = "discoverTextBox" id = "maxyear" type = "text" />
-                        </div>
-                        <div className = "discoverButtonContainer">
-                            <span>&nbsp;</span><br/>
-                            <button className = "discoverApplyButton bodytext">Apply</button>
-                        </div>
-                        
+            <div className = "discoverSearch" ref = {launchRef}>
+                <div className = "discoverInputContainer">
+                    <div className = "discoverSearchInput">
+                        <label for = "keyword" className = "discoverLabel bodytext">Keywords</label>
+                        <input className = "discoverTextBox" id = "keyword" type = "text" placeholder = "eg Falcon" onChange = {handleKeywordChange}/>
+                    </div>
+                    <div className = "discoverSearchInput">
+                        <label for = "launchpad" className = "discoverLabel bodytext">Launchpad</label>
+                        <select className = "discoverTextBox" id = "launchpad" type = "text" onChange = {handleLPChange}>
+                            {launchPads.map((launchpad) => (
+                                <option value = {launchpad.full_name}>
+                                    {launchpad.full_name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className = "discoverSearchInput">
+                        <label for = "minyear" className = "discoverLabel bodytext">Min Year</label>
+                        <input className = "discoverTextBox" id = "minyear" type = "text" />
+                    </div>
+                    <div className = "discoverSearchInput">
+                        <label for = "maxyear" className = "discoverLabel bodytext">Max Year</label>
+                        <input className = "discoverTextBox" id = "maxyear" type = "text" />
+                    </div>
+                    <div className = "discoverButtonContainer">
+                        <span>&nbsp;</span><br/>
+                        <button className = "discoverApplyButton bodytext">Apply</button>
                     </div>
                 </div>
-                {launches.map((launch) => (
-                <div>
-                    {launch.payloads.map((payload) => (
-                        <div className = "discoverResults">
+                <div className = "noOfMissionsContainer bodytext"><span>Showing {noOfMissions} Missions</span></div>
+                {filteredLaunches.map((launch, x) => (
+                <div key = {x}>
+                    {launch.payloads.map((payload, y) => (
+                        <div className = "discoverResults" key = {y}>
                             <div className = "launchText">
-                                <div className = "launchIconContainer" style = {patchStatus}>
+                                <div className = "launchIconContainer">
                                     <img className = "launchIconSrc" src = {launch.links.mission_patch} alt = "patch"/>
                                 </div>
                                 <div className = "rocketInfo">
                                     <span className = "heading5 rocketName">{launch.rocket.rocket_name}&nbsp;-&nbsp;</span><span className = "heading5 rocketType">{payload.payload_id}</span>
                                     <br/>
-                                    <span className = "rocketDateLabel bodytext">Launched: {launch.launch_date_local}</span>
+                                        <span className = "rocketDateLabel bodytext">Launched <b>{launch.launch_date_local}</b> from <span>{launchPads.filter((launchpad) => launch?.launch_site?.site_id === launchpad?.id)[0]?.full_name}</span></span>
                                 </div>
                                 <div className = "flightNumberContainer">
                                     <span className = "heading5 flightNumber">#{launch.flight_number}</span>
                                     <br/>
                                     <span className = "rocketDateLabel bodytext">Flight Number</span>
                                 </div>
-
                             </div>
                             <div className = "launchButtonContainer">
                                 <a href =  {launch.links.reddit_campaign} target = "_blank" rel = "noreferrer">
@@ -140,6 +182,8 @@ const Discover = () => {
                     ))}
                 </div>
                 ))}
+                <div className = "backToTopContainer" onClick = { () => handleClick() }><span>Back to top</span></div>
+                
             </div>
         </>
     );
